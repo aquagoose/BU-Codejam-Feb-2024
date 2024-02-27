f7700592bea8368c7fd1282286cbf0e58a2c4b32
ollie <ollie@ollierobinson.co.uk>
2024-02-27 15:14:04 +0000

    started data

---
+++ src/EcoBytes/Data/Upgrade.cs
@@ -0,0 +1,21 @@
+﻿namespace EcoBytes.Data;
+
+public readonly struct Upgrade
+{
+    public readonly string Name;
+    public readonly int Cost;
+    public readonly int BuildTime;
+    public readonly int ElecImpact;
+    public readonly int GasImpact;
+    public readonly string Description;
+
+    public Upgrade(string name, int cost, int buildTime, int elecImpact, int gasImpact, string description)
+    {
+        Name = name;
+        Cost = cost;
+        BuildTime = buildTime;
+        ElecImpact = elecImpact;
+        GasImpact = gasImpact;
+        Description = description;
+    }
+}
