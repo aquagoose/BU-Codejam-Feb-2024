19a0b61f373e38994c883f101c228e00c104743a
Thomas Turner <TJ_Turner22@outlook.com>
2024-02-28 13:02:32 +0000

    Very simple button working

--- src/EcoBytes/UI/Buttons-src/Button.cs
+++ src/EcoBytes/UI/Buttons-src/Button.cs
@@ -1,7 +1,51 @@
 using u4.Engine.Entities;
+using u4.Engine;
+using u4.Math;
+using System.Numerics;
+using System.Drawing;
+using u4.Engine.Entities;
+using u4.Render;
+using Color = u4.Math.Color;
+using EcoBytes.Components;
+using Pie.Windowing;
 
+namespace EcoBytes.UI;
 
 public class Button : Component
 {
-    
+    public event OnButtonClick ButtonClick = delegate{};
+    public Rectangle rect;
+    public Vector2 ButtonPos;
+
+    public bool Visible;
+
+    public override void Initialize()
+    {
+        Vector3 position = Transform.Position;
+        Size<int> size = Entity.GetComponent<Sprite>().Texture.Size;
+
+        rect = new Rectangle((int) position.X, (int) position.Y, size.Width, size.Height);
+    }
+
+    public override void Update(float dt)
+    {
+        Vector2 MousePos = Input.MousePosition;
+        Entity.GetComponent<Sprite>().Tint = Color.White;
+
+        if (rect.Contains((int) MousePos.X, (int) MousePos.Y))
+        {
+            Entity.GetComponent<Sprite>().Tint = new Color(1.5f,1.5f,1.5f);
+            if (Input.MouseButtonPressed(MouseButton.Left))
+            {
+                ButtonClick.Invoke();
+                Entity.GetComponent<Sprite>().Tint = new Color(0.5f,0.5f,0.5f);
+            }       
+        }
+    }
+
+    public override void Draw()
+    {
+        
+    }
+    public delegate void OnButtonClick();
 }
